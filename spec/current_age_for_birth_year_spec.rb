24103bf60d94068d661117d5dc610407cf36f063
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-08-25 04:16:50 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -3,7 +3,7 @@ require_relative '../current_age_for_birth_year.rb'
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(1984)
-  
+
 
     expect(age_of_person).to eq(19)
   end
@@ -12,5 +12,5 @@ describe "current_age_for_birth_year method" do
       2003-birth_year
     end
 
-    
+
 end
